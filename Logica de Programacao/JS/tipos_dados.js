dcf9dff44b0159be113950104018db997b7756b0
lucianojuchem <lucianojuchem@gmail.com>
2024-09-14 19:48:24 -0300

    Tipos de Dados em JS

---
+++ Logica de Programacao/JS/tipos_dados.js
@@ -0,0 +1,17 @@
+//tipo inteiro
+let numero = 30;
+//tipo ponto flutuante
+let real = 3.9;
+// Boolean
+let cidade = "Florianópolis";
+// Boolean (verdadeiro ou falso)
+let acesso = true;
+// Undefined
+let semDado;
+// Null
+let vazio = null;
+// Symbol
+let simbolo = Symbol("ID");
+
+
+console.log(numero,real,cidade,acesso,semDado,vazio,simbolo.toString())
